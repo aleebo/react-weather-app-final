aa453b61c9770a0093b820fb4f95f248501ff305
aleebo <87545942+aleebo@users.noreply.github.com>
2021-12-08 23:50:37 -0800

    Added New components; icon, info and temperature converter

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
@@ -59,6 +59,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />{" "}
+        </li>
+        <li className="text-capitalize">
+          Description: {""} {props.data.description}
+        </li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <WeatherIcon code={props.data.icon} />
+
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>{" "}
+        </div>{" "}
+        <div className="col-6">
+          <ul>
+            <li>
+              Humidity: {""}
+              {props.data.humidity}%
+            </li>
+            <li>
+              Wind: {""}
+              {props.data.wind} {""} m/h
+            </li>
+          </ul>
+        </div>
+      </div>{" "}
+    </div>
+  );
+}
