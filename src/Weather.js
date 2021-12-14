047c312173422d9361eb34ff57cf8e5b331de32d
aleebo <87545942+aleebo@users.noreply.github.com>
2021-12-13 22:24:29 -0800

    added coordinates between components and started connected them

--- src/Weather.js
+++ src/Weather.js
@@ -11,7 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      coordinatess: response.data.coord,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
@@ -61,7 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
