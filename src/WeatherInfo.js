0f34af01cd04215133b733d975c6deab8c140286
aleebo <87545942+aleebo@users.noreply.github.com>
2021-12-13 22:04:32 -0800

    create weatherForecast component and created the layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={52} />
 
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
